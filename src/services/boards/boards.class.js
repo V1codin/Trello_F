1a61c5809088e00791042c0171cf8e7b811e27db
V1codin <blindmonk46@gmail.com>
2022-06-18 15:59:01 +0300

    add experemental remove board fn

--- src/services/boards/boards.class.js
+++ src/services/boards/boards.class.js
@@ -1,4 +1,5 @@
 const { Service } = require("feathers-mongoose");
+const mongoose = require("mongoose");
 
 /*
 ? Model
@@ -113,6 +114,47 @@ exports.Boards = class Boards extends Service {
       } catch (e) {
         return Promise.reject(new Error("Invalid Board"));
       }
+    }
+  }
+
+  async remove(boardId) {
+    const session = await mongoose.startSession();
+
+    const transactionOptions = {
+      readPreference: "primary",
+      readConcern: { level: "local" },
+      writeConcern: { w: "majority" },
+    };
+    session.startTransaction(transactionOptions);
+
+    try {
+      const boardCollection = mongoose.connection.db.collection("boards");
+      const listsCollection = mongoose.connection.db.collection("lists");
+
+      await Promise.all([
+        boardCollection.deleteOne(
+          {
+            _id: mongoose.Types.ObjectId(boardId),
+          },
+          { session }
+        ),
+        listsCollection.deleteMany(
+          {
+            boardId: mongoose.Types.ObjectId(boardId),
+          },
+          { session }
+        ),
+      ]);
+
+      await session.commitTransaction();
+
+      return { _id: boardId };
+    } catch (e) {
+      console.log("======================================: ", e);
+      await session.abortTransaction();
+      return Promise.reject(new Error("Invalid Board"));
+    } finally {
+      session.endSession();
     }
   }
 
