7f0c1e1f662d89321ef8d7c3d3713f49c43e7cbe
V1codin <blindmonk46@gmail.com>
2022-06-18 16:34:37 +0300

    remove custom remove board fn:

--- src/services/boards/boards.class.js
+++ src/services/boards/boards.class.js
@@ -1,5 +1,5 @@
 const { Service } = require("feathers-mongoose");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
 /*
 ? Model
@@ -117,8 +117,9 @@ exports.Boards = class Boards extends Service {
     }
   }
 
+  /*
   async remove(boardId) {
-    const session = await mongoose.connect.startSession();
+    const session = await mongoose.startSession();
 
     const transactionOptions = {
       readPreference: "primary",
@@ -157,6 +158,7 @@ exports.Boards = class Boards extends Service {
       await session.endSession();
     }
   }
+  */
 
   // TODO remove method that removes a board and related lists and cards
   /*
