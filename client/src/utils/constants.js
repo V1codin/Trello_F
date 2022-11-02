1529e194b50fdbb2a33299bdc643a0ef94f02d7c
kir <blindmonk46@gmail.com>
2022-11-02 18:52:55 +0200

    change base url for build

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,34 +1,37 @@
-const STRATEGY = 'local';
-const GOOGLE_STRATEGY = 'google';
+const STRATEGY = "local";
+const GOOGLE_STRATEGY = "google";
 
-const STANDARD_BG = '#091921';
-const BG_IMAGE = '';
+const STANDARD_BG = "#091921";
+const BG_IMAGE = "";
 
-const BODY_REF = document.querySelector('.root');
+const BODY_REF = document.querySelector(".root");
 
 const addBoardColors = [
   {
-    name: '#003b5c',
-    backgroundColor: '#003b5c',
+    name: "#003b5c",
+    backgroundColor: "#003b5c",
   },
   {
-    name: '#7e2861',
-    backgroundColor: '#7e2861',
+    name: "#7e2861",
+    backgroundColor: "#7e2861",
   },
   {
-    name: '#27461c',
-    backgroundColor: '#27461c',
+    name: "#27461c",
+    backgroundColor: "#27461c",
   },
   {
-    name: '#4e2118',
-    backgroundColor: '#4e2118',
+    name: "#4e2118",
+    backgroundColor: "#4e2118",
   },
 ];
 
-const authFormTypeLogin = 'login';
-const authFormTypeSignup = 'signup';
+const authFormTypeLogin = "login";
+const authFormTypeSignup = "signup";
 
-const BASE_URL = 'http://localhost:3030';
+const BASE_URL =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3030"
+    : process.env.JS_ORIGINS_1;
 
 export {
   STRATEGY,
